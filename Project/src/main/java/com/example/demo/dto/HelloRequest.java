628263060f6afb5faf2af2430ada3cc834f14183
TemaFokin <artemshark1994@mail.ru>
2019-12-14 13:06:44 +0600

    Созданы dto

---
+++ Project/src/main/java/com/example/demo/dto/HelloRequest.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class HelloRequest {
+    private String greeting;
+    private String name;
+}
---
+++ Project/src/main/java/com/example/demo/dto/HelloResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class HelloResponse {
+    private String greeting;
+    private String name;
+    private Date greetingDate;
+}
